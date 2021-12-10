af24f33bb8c5e16cd751416dbc9c3034baff0dba
antonio.dyulgerski <adyulgerski94@gmail.com>
2021-12-10 14:27:18 +0200

    git ignore changes

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class About extends Component {
   render() {
     return (
-      <span className="align-center">БАЛТО ЧАКАМ ДА МИ ПРАТИШ КАКВО ЩЕ Е ТУКА !</span>
+      <span className="align-center">БАЛТО ЧАКАМ ДА МИ ПРАТИШ КАКВО ЩЕ Е ТУКА!</span>
     );
   }
 }
